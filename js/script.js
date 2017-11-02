74842bdf09566589bcd7472ff1949d31840cfd64
samwisegrangee <samuel.w.granger@gmail.com>
2017-11-02 00:01:58 -0400

    Sass commit
    Wanted to redo my site with some of the stuff I’ve learned over the
    year. I especially wanted to integrate Sass into my workflow.
    
    Unfortunately, something broke when it comes to my smooth scrolling.
    The top of my sections isn’t being recognized, so the scroll function
    misses the mark every time.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
-$(document).ready(function(){
-	// Sticky nav bar
-	var nav = $("nav");
-  var nvsp = $(".navspace");
-
-	$(window).scroll(function(){
-		if( $(this).scrollTop() > 650 ) {
-			nav.addClass("nav-scrolled", function(){
-        nvsp.addClass("no-nav-space");
-      });
-		}
-		else {
-			nav.removeClass("nav-scrolled")
-		}
-	});
-
-  //Smooth scrolling between ""#anchor" links.
+$(document).ready(function($) {
+	//Smooth scrolling between ""#anchor" links.
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
 
 	    var target = this.hash;
 	    var $target = $(target);
 
+
 	    $('html, body').stop().animate({
 	        'scrollTop': $target.offset().top
 	    }, 900, 'swing', function () {
 	        window.location.hash = target;
 	    });
+	});
+
+	// Sticky nav bar
+	var nav = $("nav");
+	var nvsp = $(".navspace");
+
+	$(window).scroll(function(){
+
+		if( $(this).scrollTop() > 650 ) {
+			nav.addClass("nav-scrolled", function(){
+				nvsp.addClass("no-nav-space");
+			});
+		}
+		else {
+			nav.removeClass("nav-scrolled")
+		}
 	});
 });
